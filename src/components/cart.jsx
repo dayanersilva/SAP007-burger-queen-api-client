11aff939e5ba57d221a4a68c12178c73f622b3ae
suelenescorcio <suelen919@gmail.com>
2022-05-21 23:49:57 -0300

    testes ok

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,28 +3,28 @@ import styles from "./components.module.css";
 
 const Cart = ({ name, flavor, complement, price, qtd, type, onClick }) => {
   return (
-    <li className={styles.order}>
-      <div className={styles.orderContainer}>
-        <p>{qtd}</p>
-        <div className={styles.orderInformation}>
-          <h4>{name}</h4>
-          {type === "hamburguer" ? (
-            <section>
-              <p value="sabor">- {flavor}</p>
-              <p name="complemento">
-                - {`${complement !== null ? complement : ""}`}
-              </p>
-            </section>
-          ) : (
-            ""
-          )}
+      <li className={styles.order}>
+        <div className={styles.orderContainer}>
+          <p>{qtd}</p>
+          <div className={styles.orderInformation}>
+            <h4>{name}</h4>
+            {type === "hamburguer" ? (
+              <section>
+                <p value="sabor">- {flavor}</p>
+                <p name="complemento">
+                  - {`${complement !== null ? complement : ""}`}
+                </p>
+              </section>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-      </div>
-      <p>R$ {price},00</p>
-      <button className="add-btn" onClick={onClick}>
-        Excluir item
-      </button>
-    </li>
+        <p>R$ {price},00</p>
+        <button className="add-btn" onClick={onClick}>
+          Excluir item
+        </button>
+      </li>
   );
 };
 
--- src/components/orderCards.jsx
+++ src/components/orderCards.jsx
@@ -13,16 +13,6 @@ const nameButton = (status) => {
   }
 };
 
-// const initialStatus = (status) => {
-//   if (status === 'pending') {
-//     return 'Pendente';
-//   } else if (status === 'preparando') {
-//     return 'Preparando';
-//   } else {
-//     return 'Finalizado';
-//   }
-// };
-
 const OrderCard = ({
   id,
   name,
